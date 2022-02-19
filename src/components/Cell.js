8cbea5b4c05b4afeddec9f2f66db6cc1417ac6ab
boonjhee <wongboonjhee@DESKTOP-7K259QB.localdomain>
2022-02-19 23:21:24 +0800

    added styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {StyledCell} from './styles/StyledCell'
+import {TETROMINOS} from '../tetrominos';
 
 const Cell = ({type}) => (
-    <div>
+    <StyledCell type={type} color={TETROMINOS[type].color}>
         cell
-    </div>
+    </StyledCell>
 );
 
 export default Cell;
