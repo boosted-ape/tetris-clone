e134e9e2e401eca152938135a60ded1a342e862e
DESKTOP-7K259QB\Owner <wbjhee@gmail.com>
2022-03-01 20:33:56 +0800

    added useStage hook

--- src/components/hooks/useStage.js
+++ src/components/hooks/useStage.js
@@ -9,8 +9,25 @@ export const useStage = (player, resetPlayer) => {
         const updateStage = prevStage => {
             //flush the stage
             const newStage = prevStage.map(row =>
-                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)))
-        }
-    })
+                row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+            );
+
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0){
+                        newStage[y + player.pos.y][x + player.pos.x] = [
+                            value,
+                            `${player.collided ? "merged" : "clear"}`,
+                        ];
+                    }
+                });
+            });
+
+            return newStage;
+            
+        };
+
+        setStage( prev => updateStage(prev));
+    }, [player.collided, player.pos.x, player.pos.y, player.tetriomino]);
     return [stage, setStage];
 }
