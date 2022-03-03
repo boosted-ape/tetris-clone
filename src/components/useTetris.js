4273164dfdbf556515dc7a33aef352cdc0d6bb56
DESKTOP-7K259QB\Owner <wbjhee@gmail.com>
2022-03-03 21:46:10 +0800

    added piece rendering

--- src/components/useTetris.js
+++ src/components/useTetris.js
@@ -8,5 +8,31 @@ export const useTetris = (canvasRef) => {
 
         context.fillStyle = '#000';
         context.fillRect(0, 0, 240, 400);
+
+        const matrix = [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+
+        context.scale(20, 20);
+
+        function drawMatrix(matrix, offset) {
+            matrix.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        context.fillStyle = 'red';
+                        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+                    }
+                });
+            });
+        };
+        const player = {
+            pos: {x: 5, y: 5},
+            matrix: matrix,
+        }
+
+        drawMatrix(player.matrix, player.pos)
+
     });
 }
