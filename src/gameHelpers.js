efb76fc6d0c139b07488977aacce9bb87b416499
DESKTOP-7K259QB\Owner <wbjhee@gmail.com>
2022-03-03 11:37:33 +0800

    changed directories

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -4,4 +4,16 @@ export const STAGE_HEIGHT = 20;
 export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT),() => 
     new Array(STAGE_WIDTH).fill([0,'clear']))
+
+export const checkCollision = (player,stage) => {
+    for(let y = 0; y < player.tetromino.length; y++){
+        for(let x = 0; x < player.tetromino[y].length; x++){
+            //check that we're on a tetromino cell
+            if (player.tetromino[y][x] !== 0){
+                //check that we're not out of bounds
+                if(!stage[y + player.pos.y +]){}
+            }
+        }
+    }
+}
     
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
-import { TETROMINOS, randomTetromino } from '../../tetrominos';
-import { STAGE_WIDTH } from '../../gameHelpers';
+import { TETROMINOS, randomTetromino } from '../tetrominos'
+import { STAGE_WIDTH } from '../gameHelpers'
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -10,13 +10,12 @@ export const usePlayer = () => {
     collided: false,
   });
 
-  const updatePlayerPos = ({ x, y, collided }) => {
+  const updatePlayerPos = ({x, y, collided}) => {
     setPlayer(prev => ({
-      ...prev,
-      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
-      collided,
+        ...prev,
+        pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)}, collided: collided
     }))
-  }
+}
 
   const resetPlayer = useCallback(() => {
     setPlayer({
